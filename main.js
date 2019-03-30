34823b7cbf9bf3bb2b374b49062ef56eab35155e
Jonas Karg <jonas.karg@hotmail.com>
2019-03-30 11:11:01 +0100

    Now working. Experimanting with some stuff..

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 const cnv = document.querySelector("canvas");
 const ctx = cnv.getContext("2d");
 
-function point(x, y, n) {
-  let color = "blue";
+function sigmoid(x) {
+  return 1 / (1 + Math.pow(Math.e, -x));
+}
 
-  if (n <= 3) {
-    color = "black";
-  } else if (n === 4) {
-    color = "red";
-  } else if (n === 5) {
-    color = "orange";
-  } else if (n === 6) {
-    color = "orange";
-  } else {
-    color = "blue";
-  }
+const TWO_PI = Math.PI * 2;
+const QUARTER_PI = Math.PI / 4;
+const EIGHT_PI = Math.PI / 8;
+
+function getColor(_x) {
+  const x = _x * TWO_PI;
+
+  return {
+    r: 255 - Math.abs(Math.sin(x) * 255),
+    g: 255 - Math.abs(Math.sin(x + EIGHT_PI) * 255),
+    b: 255 - Math.abs(Math.sin(x + QUARTER_PI) * 255)
+  };
+}
+
+function point(x, y, growthSpeed) {
+  // let color = "blue";
+  const _color = getColor(Math.log(growthSpeed) / 500);
+  const color = `rgb(${ _color.r }, ${ _color.g }, ${ _color.b })`;
+
+  // if (growthSpeed <= 0.05) {
+  //   color = "black";
+  // } else if (growthSpeed <= 0.1) {
+  //   color = "red";
+  // } else if (growthSpeed <= 0.5) {
+  //   color = "orange";
+  // } else if (growthSpeed <= 3) {
+  //   color = "yellow";
+  // } else if (growthSpeed <= Math.pow(10, 40)) {
+  //   color = "darkblue";
+  // }
+
+
 
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
@@ -23,8 +45,8 @@ function point(x, y, n) {
 // My implementation of a complex number
 class Complex {
   constructor(r, i) {
-    this.r = r; // Real part
-    this.i = i; // Imaginary part
+    this.r = r; // Real component
+    this.i = i; // Imaginary component
   }
 
   // Multiplication of two complex numbers
@@ -35,16 +57,18 @@ class Complex {
     );
   }
 
+  distance(other) {
+    return Math.sqrt(
+      Math.pow(this.r - other.r, 2) +
+      Math.pow(this.i - other.i, 2)
+    );
+  }
+
   add(other) {
     return new Complex(
       this.r + other.r,
       this.i + other.i
     );
-  }
-
-  equals(other) {
-    return this.r === other.r
-      && this.i === other.i;
   }
 }
 
@@ -59,58 +83,52 @@ function z(values, c, depth = 0) {
   return z(values, c, depth);
 }
 
-function getN(c) {
-  const first10 = z([new Complex(0, 0)], c, 10);
+function getGrowthSpeed(c) {
+  const firstFew = z([new Complex(0, 0)], c, 10);
+
+  // The distances between the first and all of
+  // the other points
+  const distances = [];
 
   // Compare first value to all others
-  // for (let n = 1; n < 10; n++) {
-    // if (first10[0].equals(first10[n])) {
-      // return n;
-    // }
-  // }
-
-  // Compare every value to every other
-  for (let n = 0; n < 10; n++) {
-    for (let k = 0; k < 10; k++) {
-      if (n === k) continue;
-
-      if (first10[n].equals(first10[k])) {
-        return Math.abs(n - k);
-      }
-    }
+  for (let n = 1; n < firstFew.length; n++) {
+    distances.push(firstFew[0].distance(firstFew[n]));
   }
 
-  return 10;
+  const deltas = [];
+
+  for (let n = 1; n < distances.length; n++) {
+    deltas.push(distances[n] - distances[n - 1]);
+  }
+
+  const average = deltas.reduce((a, b) => (a + b)) / deltas.length;
+
+  // console.log(average)
+
+  return Math.abs(average);
 }
 
-const WIDTH = 10;
-const HEIGHT = 10;
-const HALF_WIDTH = WIDTH / 2;
-const HALF_HEIGHT = HEIGHT / 2;
+const WIDTH = cnv.width;
+const HEIGHT = cnv.height;
 
-console.log(getN(new Complex(
-  -1, 0
-)));
+const SCALE_X = 3;
+const SCALE_Y = 3;
+const OFFSET_X = -0.2;
+const OFFSET_Y = 0;
+function draw() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-// Problem:
-// Gives 10 for almost all the points in the c-plane.
-// Probably wrong scaling...
+  for (let y = 0; y < WIDTH; y++) {
+    for (let x = 0; x < HEIGHT; x++) {
+      point(
+        x, y,
+        getGrowthSpeed(new Complex(
+          ((x / WIDTH + OFFSET_X) * SCALE_X) - SCALE_X / 2,
+          ((y / HEIGHT + OFFSET_Y) * SCALE_Y) - SCALE_Y / 2
+        ))
+      );
+    }
+  }
+}
 
-// function draw() {
-//   for (let x = 0; x < WIDTH; x++) {
-//     for (let y = 0; y < HEIGHT; y++) {
-//       console.log(getN(new Complex(
-//         x / 100000 - .5,
-//         y / 100000 - .5
-//       )));
-
-//       // point(
-//         // x,
-//         // y,
-//         // getN(new Complex(x / WIDTH * 5, y / HEIGHT * 5))
-//       // );
-//     }
-//   }
-// }
-
-// window.requestAnimationFrame(draw);
+window.requestAnimationFrame(draw);
