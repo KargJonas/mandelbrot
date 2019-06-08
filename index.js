f57159a6ac541a075734b48588da3fa4852cbb75
Jonas Karg <jonas.karg@hotmail.com>
2019-06-08 16:44:17 +0200

    In the process of compressing

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const precisionInput = document.querySelector("input");
-const cnv = document.querySelector("canvas");
-const gl = cnv.getContext("webgl") || cnv.getContext("experimental-webgl");
-const shaderProgram = gl.createProgram();
-const vertexShaderText = `
-attribute vec2 position;
-void main(void) {
-  gl_Position = vec4(position, 0, 1);
-}`;
+let p = document.querySelector("input");
+let c = document.querySelector("canvas");
+let g = c.getContext("webgl")||c.getContext("experimental-webgl");
+let s = g.createProgram();
 
-let zoom = 0.3;
+let z = 0.3;
 
 class Vector {
   constructor(x, y) {
@@ -45,20 +40,17 @@ class Vector {
   }
 }
 
-/**
- * Not a conventional mouse!
- * Click-to drag. Position can
- * be outside of the document.
- */
+let e = addEventListener;
+
 class Mouse {
   constructor() {
     this.pos = new Vector(0, 0);
     this.last = new Vector(0, 0);
     this.isDown = false;
 
-    addEventListener("mousedown", (e) => this.down(e));
-    addEventListener("mouseup", () => this.up());
-    addEventListener("mousemove", (e) => this.move(e));
+    e("mousedown", (e) => this.down(e));
+    e("mouseup", () => this.up());
+    e("mousemove", (e) => this.move(e));
   }
 
   down(e) {
@@ -75,86 +67,48 @@ class Mouse {
 
   move(e) {
     if (this.isDown) {
-      change();
+      b();
 
       const current = new Vector(
         1 - e.clientX / innerWidth,
         e.clientY / innerHeight
       );
 
-      const delta = current.sub(this.last);
-      this.pos = this.pos.add(delta.div(zoom));
+      let delta = current.sub(this.last);
+      this.pos = this.pos.add(delta.div(z));
       this.last = current.clone();
     }
   }
 }
 
-function change() {
-  cnv.width = innerWidth;
-  cnv.height = innerHeight;
-  gl.uniform2f(gl.getUniformLocation(shaderProgram, "resolution"), innerWidth, innerHeight);
-  gl.uniform2f(gl.getUniformLocation(shaderProgram, "mousePos"), mouse.pos.x, mouse.pos.y);
-  gl.uniform1f(gl.getUniformLocation(shaderProgram, "zoom"), zoom);
-  gl.uniform1i(gl.getUniformLocation(shaderProgram, "steps"), Math.pow(precisionInput.value, 2));
+function b() {
+  c.width = innerWidth;
+  c.height = innerHeight;
+  g.uniform1f(g.getUniformLocation(s,"h"),innerHeight);
+  g.uniform2f(g.getUniformLocation(s,"m"),m.pos.x,m.pos.y);
+  g.uniform1f(g.getUniformLocation(s,"o"),z);
+  g.uniform1i(g.getUniformLocation(s,"s"),p.value*p.value);
+  g.viewport(0, 0, innerWidth, innerHeight);
 }
 
-const mouse = new Mouse();
-addEventListener("mousewheel", (e) => {
-  if (e.wheelDelta < 0) zoom /= 1.05;
-  else zoom *= 1.05;
-  change();
-});
+let m=new Mouse();
+e("mousewheel",v=>{z*=v.wheelDelta<0?.95:1.05;b()});
+e("resize",b);p.addEventListener("change",b);
 
-addEventListener("resize", () => {
-  change();
-  gl.viewport(0, 0, innerWidth, innerHeight);
-});
-
-precisionInput.addEventListener("change", change);
-
-function update() {
-  gl.drawArrays(5, 0, 4);
-  requestAnimationFrame(update);
+function u() {
+  g.drawArrays(5, 0, 4);
+  requestAnimationFrame(u);
 }
 
 function generateShader(type, shaderText) {
-  const shader = gl.createShader(type);
+  const S = g.createShader(type);
 
-  gl.shaderSource(shader, shaderText);
-  gl.compileShader(shader);
+  g.shaderSource(S, shaderText);
+  g.compileShader(S);
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(shader));
+  if (!g.getShaderParameter(S, g.COMPILE_STATUS)) {
+    console.error(g.getShaderInfoLog(S));
   }
-
-  gl.attachShader(shaderProgram, shader);
+  g.attachShader(s, S);
 }
-
-async function run() {
-  const fragmentShaderText = await fetch("shader.glsl")
-    .then((response) => (response.text()));
-
-  generateShader(gl.VERTEX_SHADER, vertexShaderText);
-  generateShader(gl.FRAGMENT_SHADER, fragmentShaderText);
-
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
-
-  const vertices = [
-    -1, -1,
-    -1, +1,
-    +1, -1,
-    +1, +1
-  ];
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(0);
-  gl.viewport(0, 0, innerWidth, innerHeight);
-
-  change();
-  update();
-}
-
-run();
+generateShader(35633,`attribute vec2 p;void main(){gl_Position=vec4(p,0,1);}`);generateShader(35632,`precision mediump float;uniform float h;uniform vec2 m;uniform float o;uniform int s;vec2 z(vec2 c){vec2 n=vec2(0.,0.);for(int i=0;i<3600;i++){if(i>s)break;n=vec2(n.x*n.x-n.y*n.y,2.*(n.x*n.y))+c;}return n;}vec3 g(vec2 p){float x=log(length(z(p)));return vec3(abs(sin(x)),abs(sin(x+.39)),abs(sin(x+.78)));}void main(){vec2 p=gl_FragCoord.xy/h;p-=.5;float x=log(length(z(p/o+m)));vec3 c=vec3(abs(sin(x)),abs(sin(x+.39)),abs(sin(x+.78)));gl_FragColor=vec4(c,1.);}`);g.linkProgram(s);g.useProgram(s);g.bindBuffer(34962,g.createBuffer());g.bufferData(34962,new Float32Array([-1,-1,-1,1,1,-1,1,1]),35044);g.vertexAttribPointer(0,2,5126,false,0,0);g.enableVertexAttribArray(0);g.viewport(0,0,innerWidth,innerHeight);b();u();
